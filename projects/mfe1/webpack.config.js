b68a836225620c180bde121d38c721ad238d3cff
Amaresh <amareshs@gluedin.io>
2023-07-27 15:17:27 +0530

    Shared Dependancy

--- projects/mfe1/webpack.config.js
+++ projects/mfe1/webpack.config.js
@@ -1,20 +1,41 @@
+// Import share instead of shareAll:
 const {
-  shareAll,
+  share,
   withModuleFederationPlugin,
 } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
   name: 'mfe1',
+
   exposes: {
     './FlightsModule': './projects/mfe1/src/app/flights/flights.module.ts',
     './LazyComponent': './projects/mfe1/src/app/flights/lazy/lazy.component.ts',
     './PublicModule': './projects/mfe1/src/public.d.ts'
   },
-  shared: {
-    ...shareAll({
+  // Explicitly share packages:
+  shared: share({
+    '@angular/core': {
       singleton: true,
       strictVersion: true,
       requiredVersion: 'auto',
-    }),
-  },
+    },
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common/http': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/router': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+  }),
+
+  // Explicitly share mono-repo libs:
+  sharedMappings: ['auth-lib'],
 });
--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,49 +1,39 @@
+// Import share instead of shareAll:
 const {
-  shareAll,
   share,
   withModuleFederationPlugin,
-  SharedMappings
 } = require("@angular-architects/module-federation/webpack");
-const sharedMappings = new SharedMappings();
-
 
 module.exports = withModuleFederationPlugin({
   remotes: {
+    // Check this line. Is port 4201 configured?
     mfe1: "http://localhost:4201/remoteEntry.js",
   },
 
-  shared: {
-    ...shareAll({
+  // Explicitly share packages:
+  shared: share({
+    "@angular/core": {
       singleton: true,
       strictVersion: true,
-      requiredVersion: 'auto',
-    }),
-  },
+      requiredVersion: "auto",
+    },
+    "@angular/common": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/common/http": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/router": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+  }),
 
-  // this specifies the shared libraries so that these libraries are singelon instances -
-  // meaning that every application consumes the same:
-
-  // shared: share({
-  //   "@angular/core": {
-  //     singleton: true,
-  //     strictVersion: true,
-  //     requiredVersion: "auto",
-  //   },
-  //   "@angular/common": {
-  //     singleton: true,
-  //     strictVersion: true,
-  //     requiredVersion: "auto",
-  //   },
-  //   "@angular/common/http": {
-  //     singleton: true,
-  //     strictVersion: true,
-  //     requiredVersion: "auto",
-  //   },
-  //   "@angular/router": {
-  //     singleton: true,
-  //     strictVersion: true,
-  //     requiredVersion: "auto",
-  //   },
-  //   ...sharedMappings.getDescriptors(),
-  // }),
+  // Explicitly share mono-repo libs:
+  sharedMappings: ["auth-lib"],
 });
